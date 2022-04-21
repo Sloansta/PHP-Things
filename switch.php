1a953410b39f50bb015edf4214fb191ef49b333b
Sloan <Sloan.boyce@gmail.com>
2022-04-21 19:57:56 -0400

    switch

--- switch.php
+++ switch.php
@@ -7,6 +7,23 @@
     <title>PHP Switch</title>
 </head>
 <body>
-    
+    <?php
+        $age = 12;
+        switch(true) {
+            case ($age < 5):
+                echo "Stay home<br>";
+                break;
+            case ($age == 5):
+                echo "Go to Kindergarten<br>";
+                break;
+            case in_array($age, range(6, 17)):
+                $grade = $age - 5;
+                echo "Go to Grade $grade<br>";
+                break;
+            default:
+                echo "Go to College<br>";
+                break;
+        }
+    ?>
 </body>
 </html>
