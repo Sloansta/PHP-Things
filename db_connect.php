cef0f1ecb821f9bce1136271ef1243f9444cda51
Sloan <Sloan.boyce@gmail.com>
2022-04-28 20:49:23 -0400

    Learned how PHP can manipulate a database

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php 
+    DEFINE('DB_USER', 'root');
+    DEFINE('DB_PASSWORD', '!');
+
+    $dsn = 'mysql:host=localhost;dbname=students';
+    try {
+        $db = new PDO($dsn, DB_USER,  DB_PASSWORD);
+    } catch(PDOException $e) {
+        $err_msg = $e->getMessage();
+        include('db_err.php');
+        exit();
+    }
+?>
